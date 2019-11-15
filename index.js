5a61ed51806f57d72e69ed47fdd30ac5b9e622c2
halil-ibrahim-baykan <halilibrahimbaykan@gmail.com>
2019-11-15 10:08:26 +0100

    server run

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+const messages = [];
+// const jsonparser= bodyParser.json()
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => res.send("hii"));
+
+app.post("/message", (req, res) => {
+  const { message } = req.body; //bunu sor
+  messages.push(message);
+  res.send(message);
+});
+
 port = 4000;
 app.listen(port, () => console.log("Server is running on port:" + port));

ed929c808e91f1100f4a9039c1f5e0ddec68f91d
halil-ibrahim-baykan <halilibrahimbaykan@gmail.com>
2019-11-15 10:38:30 +0100

    stram connection endpoint

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const Sse = require("json-sse");
 
 const app = express();
-
 const messages = [];
 // const jsonparser= bodyParser.json()
+
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("hii"));
@@ -15,6 +16,17 @@ app.post("/message", (req, res) => {
   // const message = req.body.message
   messages.push(message);
   res.send(message);
+});
+
+const stream = new Sse();
+//stream clients connected with stream. we make stream and connect client with stream when we send data sstream everyone reach them
+//serieles = turn our data series of caractrers
+app.get("/stream", (req, res, next) => {
+  const string = JSON.stringify(messages);
+
+  stream.updateInit(string);
+
+  stream.init(req, res);
 });
 
 port = 4000;
